66fcd76ebb8721fc80d64e7e914f49394444421c
Samuel Yazie <samuel.yazie@abmtax.ca>
2026-04-15 16:27:00 -0500

    adding html and css

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict'
 
+let userName;
+userName = window.prompt("What is your name?");
+console.log(`Hello to the world of coding ${userName}`);
+
+let title = document.getElementById('main-title');
+title.innerText = `Hello to the world of coding ${userName}`;
+
 console.log("Hello world");
 
 let greeting = "Hello, World!";
