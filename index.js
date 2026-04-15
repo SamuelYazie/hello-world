122a651ec1fc41ab1cd7bfc0db46330488c74e20
Samuel Yazie <samuel.yazie@abmtax.ca>
2026-04-15 15:41:27 -0500

    some fun additions to my hello world

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict'
 
+console.log("Hello world");
+
+
 let greeting = "Hello, World!";
 console.log(greeting);
+
+let name = "Samuel";
+console.log(`Hello world, my name is ${name}`);
+
+
