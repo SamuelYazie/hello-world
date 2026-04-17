b597271834728bb67b75d5b62cfc3a52210cf30f
Samuel Yazie <samuel.yazie@abmtax.ca>
2026-04-17 15:13:03 -0500

    adding background soundeffect

--- script/index.js
+++ script/index.js
@@ -1,11 +1,17 @@
 'use strict'
 
+const codingSound = document.getElementById('coding-Sound');
 let userName;
 userName = window.prompt("What is your name?");
 console.log(`Hello to the world of coding ${userName}`);
 
 let title = document.getElementById('main-title');
 title.innerText = `Hello to the world of coding ${userName}`;
+
+document.addEventListener('click', () => {
+    codingSound.play();
+    codingSound.volume = 0.8;
+});
 
 console.log("Hello world");
 
